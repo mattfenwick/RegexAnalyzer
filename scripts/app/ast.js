5e588ad16166e6478db0ba921e671abce87cab00
Matt Fenwick <mfenwick100@gmail.com>
2013-02-12 00:31:24 +0100

    ast: tests and avoid constructors that require 'new'

--- scripts/app/ast.js
+++ scripts/app/ast.js
@@ -15,31 +15,42 @@ define(function () {
     function isChar(x) {
         return typeof x === 'string' && x.length === 1;
     }
+    
+    function isArray(x) {
+        var isNot = x.length === undefined || typeof x === 'string';
+        return !isNot;
+    }
 
     function char(chr, q) {
         if(!(isChar(chr) && isQuant(q))) {
             throw new Error('type error');
         }
-        this.asttype = 'char';
-        this.value = chr;
-        this.quantifier = q;
+        return {
+            asttype: 'char',
+            value: chr,
+            quantifier: q
+        };
     }
 
     function dot(q) {
         if(!isQuant(q)) {
             throw new Error('type error');
         }
-        this.asttype = 'dot';
-        this.quantifier = q;
+        return {
+            asttype: 'dot',
+            quantifier: q
+        };
     }
 
     function backref(int, q) {
         if(!(typeof int === 'number' && isQuant(q))) {
             throw new Error('type error');
         }
-        this.asttype = 'backref';
-        this.value = int;
-        this.quantifier = q;
+        return {
+            asttype: 'backref',
+            value: int,
+            quantifier: q
+        };
     }
 
     var ANCHORS = {
@@ -56,39 +67,47 @@ define(function () {
         if(!isQuant(q)) {
             throw new Error('type error');
         }
-        this.asttype = 'anchor';
-        this.value = ANCHORS[a];
-        this.quantifier = q;
+        return {
+            asttype: 'anchor',
+            value: ANCHORS[a],
+            quantifier: q
+        };
     }
 
-    function any(negate, phrases, q) {
-        if(!(typeof negate === 'boolean' && isArray(phrases) && isQuant(q))) {
+    function any(isNegated, phrases, q) {
+        if(!(typeof isNegated === 'boolean' && isArray(phrases) && isQuant(q))) {
             throw new Error('type error');
         }
-        this.asttype = 'any';
-        this.negate = negate;
-        this.phrases = phrases;
-        this.quantifier = q;
+        return {
+            asttype: 'any',
+            isNegated: isNegated,
+            phrases: phrases,
+            quantifier: q
+        };
     }
 
     function group(isCapture, phrases, q) {
         if(!(typeof isCapture === 'boolean' && isArray(phrases) && isQuant(q))) {
             throw new Error('type error');
         }
-        this.asttype = 'group';
-        this.isCapture = isCapture;
-        this.phrases = phrases;
-        this.quantifier = q;
+        return {
+            asttype: 'group',
+            isCapture: isCapture,
+            phrases: phrases,
+            quantifier: q
+        };
     }
 
     function range(low, high, q) {
         if(!(isChar(low) && isChar(high) && isQuant(q))) {
             throw new Error('type error');
         }
-        this.asttype = 'range';
-        this.low = low;
-        this.high = high;
-        this.quantifier = q;
+        return {
+            asttype: 'range',
+            low: low,
+            high: high,
+            quantifier: q
+        };
     }
 
     function quantifier(low, high, isGreedy) {
--- scripts/test/app/ast.js
+++ scripts/test/app/ast.js
@@ -1,6 +1,6 @@
 define(["app/ast"], function (AST) {
 
-    return function() {
+    return function(expExc) {
     
         module("ast");
         var one = AST.quantifier(1, 1, true);
@@ -11,8 +11,29 @@ define(["app/ast"], function (AST) {
         });
         
         test("char", function() {
-            propEqual({type: 'astnode', value: '\n', quantifier: {type: 'astnode', low: 1, high: 1, isGreedy: true}},
-                AST.char('x', one));
+            propEqual({asttype: 'char', value: '\n', quantifier: {asttype: 'quantifier', low: 1, high: 1, isGreedy: true}},
+                AST.char('\n', one));
+        });
+        
+        test("dot", function() {
+            propEqual({asttype: 'dot', quantifier: {asttype: 'quantifier', low: 1, high: 1, isGreedy: true}},
+                AST.dot(one));
+        });
+        
+        test("any", function() {
+            propEqual({asttype: 'any', isNegated: false, phrases: [AST.char('3', one)], quantifier: one},
+                AST.any(false, [AST.char('3', one)], one));
+        });
+        
+        test("group", function() {
+            propEqual({asttype: 'group', isCapture: true, phrases: [AST.dot(one)], quantifier: one},
+                AST.group(true, [AST.dot(one)], one));
+        });
+        
+        test("bad data -> exception", function() {
+            expExc(function() {
+                AST.range('a', 13, one);
+            });
         });
     };
 });
